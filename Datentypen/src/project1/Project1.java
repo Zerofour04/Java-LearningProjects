56966745aa746fd310ff252c53d636bd7bb5f176
Ben // Zerofour // 04 <brubiyt@gmail.com>
2022-11-10 23:10:54 +0100

    First upload

---
+++ Datentypen/src/project1/Project1.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+Ho, Ben FT12D
+ */
+package project1;
+
+/**
+ *
+ * @author hobe218901
+ */
+public class Project1 {
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Author: Ben / Zerofour");
+        
+    int anzahl = 48;
+    System.out.println("Ben hat " + anzahl + " Projekte.");
+    
+    System.out.println("Java ist eine... \t\"vielseitige\" Programmiersprache");
+    System.out.println("in der auch");
+    System.out.println("\t\t'zahlreiche'");
+    System.out.println("\t\t\tMöglichkeiten");
+    System.out.println("\t\t\tzur Textformatierung");
+    System.out.println("\t\tmit Hilfe von \"\\-Sequenzen\"");
+    System.out.println("bereitstehen.");
+    }
+}
---
+++ mwst/src/mwst/Mwst.java
@@ -0,0 +1,39 @@
+/*
+ * Programm zum Berechnen der Mehrwertsteuer
+ * Zur Bearbeitung im Unterricht markierten Teil entfernen.
+ */
+package mwst;
+import java.util.Scanner;   // Einbinden zum Verwenden des Scanners
+
+public class Mwst {
+
+      
+    public static double round2Dec(double x)    //mathematisch auf 2 Dezimalen runden
+    {
+        x = Math.round(x*100);
+        return(x/100);
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double mwstSatz;        // Variable zum Speichern des MwSt-Satzes
+        double nettoBetrag;     // Variable zum Speichern des Netto-Betrages
+        double bruttoBetrag;    // Variable zum Speichern des Brutto-Betrages
+        Scanner myScanner = new Scanner(System.in);
+        
+        System.out.println("Bitte Nettobetrag eingeben:");
+        nettoBetrag = myScanner.nextDouble(); // Einlesen des Nettobetrages
+        
+        /* HIER Vervollstaendigen */
+        
+        System.out.println("Bitte Prozente eingeben:");
+        mwstSatz = myScanner.nextDouble();
+        bruttoBetrag =  nettoBetrag * ( 1 + mwstSatz / 100); 
+        bruttoBetrag = round2Dec(bruttoBetrag);
+        
+        System.out.println("Der Bruttobetrag ist " + bruttoBetrag + "€");
+    }
+    
+}
